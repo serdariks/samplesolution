9f2353da16838de8266d0d97aa691adbd2d1158f
s.kizilkurt <s.kizilkurt@triodor.eu>
2019-02-21 10:35:22 +0300

    first commit

---
+++ dotnet-service-one/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using triodor_messaging_sockets;
+
+namespace dotnet_service_one
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            new Class1().Test();
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ triodor-messaging-sockets/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace triodor_messaging_sockets
+{
+    public class Class1
+    {
+        public void Test(){
+            Console.WriteLine("Hello from class lib");
+        }
+    }
+}
